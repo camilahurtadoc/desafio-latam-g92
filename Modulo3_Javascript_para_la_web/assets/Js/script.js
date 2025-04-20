7e09670fe05030dbf68a418b7c76e426995fc6a4
Camila Hurtado <camilahurtadoc@gmail.com>
2025-04-20 19:39:12 -0400

    completa desafio

--- Modulo3_Javascript_para_la_web/assets/Js/script.js
+++ Modulo3_Javascript_para_la_web/assets/Js/script.js
@@ -1,4 +1,23 @@
-precio = 400000
+const precio = 400000;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+const precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let cantidadTotal = document.querySelector('.cantidad');
+
+let valorTotal = document.querySelector('.valor-total');
+
+valorTotal.innerHTML = cantidadTotal.innerHTML * precio;
+
+const btnmas = document.querySelector('.btnmas');
+
+const btnmenos = document.querySelector('.btnmenos');
+
+
+btnmas.addEventListener("click", () => {
+    valorTotal.innerHTML = Number(cantidadTotal.innerHTML) * Number(precio);
+})
+
+btnmenos.addEventListener("click",() => {
+    valorTotal.innerHTML = Number(cantidadTotal.innerHTML) * Number(precio);
+})
