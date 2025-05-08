54f08ff238d40ceb144b20a865459c9e835daf98
Camila Hurtado <camilahurtadoc@gmail.com>
2025-05-08 15:50:44 -0400

    respuestas 2.1  2.2  y  2.3

--- Modulo3_Javascript_para_la_web/desafio_3/assets/Js/script.js
+++ Modulo3_Javascript_para_la_web/desafio_3/assets/Js/script.js
@@ -1,9 +1,35 @@
-// PREGUNTA 2.1
+// ----------------------------------
+// PREGUNTA 2.1 - Separar html y javascript
 // ----------------------------------
 
 function pintar(){
-    ele.style.backgroundColor = 'yellow'
+    ele.style.backgroundColor = 'yellow';
 }
     
-const ele = document.getElementById("ele1")
-ele.addEventListener("click", pintar);
+const ele = document.getElementById("ele1");
+//Se comenta la linea 10 para poder correr los siguientes ítems del desafío
+//ele.addEventListener("click", pintar);
+
+
+// ----------------------------------
+// PREGUNTA 2.2 - Modificar función para que reciba elemento clickeado
+// ----------------------------------
+
+function pintar2(elementoClickeado) {
+    elementoClickeado.style.backgroundColor = 'yellow';
+}
+
+//Se comenta linea 23 para funcionalidad del código que sigue
+//ele.addEventListener('click', () => pintar2(ele))
+
+
+// ----------------------------------
+// PREGUNTA 2.3 - Modificar función para que reciba también un color
+// ----------------------------------
+
+function pintar3(elementoClickeado, color = 'green') {
+    elementoClickeado.style.backgroundColor = color;
+}
+
+ele.addEventListener('click', () => pintar3(ele, 'yellow'));
+
