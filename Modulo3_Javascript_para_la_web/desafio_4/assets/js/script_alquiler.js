2ffe9571513fa4b23ae816ae807d6b5135ed8335
Camila Hurtado <camilahurtadoc@gmail.com>
2025-05-17 22:15:58 -0400

    agrega scripts para venta y alquiler

---
+++ Modulo3_Javascript_para_la_web/desafio_4/assets/js/script_alquiler.js
@@ -0,0 +1,52 @@
+const propiedades_alquiler = [
+{
+nombre: 'Lujoso departamento en rascacielos',
+id: '7289',
+descripcion: 'Amplio y moderno departamento con doble techo y vista incomparable',
+ubicacion: 'Camino al cielo 9823, Distrito 0',
+habitaciones: 3,
+costo: 3400000,
+smoke: false,
+pets: false
+},
+{
+nombre: 'Departamento en barrio artístico',
+id: '14735',
+descripcion: 'Bello departamento en pintoresco edificio ubicado en el centro del barrio artístico',
+ubicacion: 'Avenida del arquitecto 6699, Distrito 5',
+habitaciones: 2,
+costo: 600000,
+smoke: true,
+pets: false
+},
+{
+nombre: 'Casa a las afueras de la ciudad',
+id: '16157',
+descripcion: 'Amplia casa en sector tranquilo con amplio terreno',
+ubicacion: 'Camino escondido 3256, Distrito 7',
+habitaciones: 7,
+costo: 5500000,
+smoke: true,
+pets: true
+},
+{
+nombre: 'Casita en la playa',
+id: '19410',
+descripcion: 'Pequeña casita en la playa, perfecta para vacaciones en pareja',
+ubicacion: 'Calle en la arena 23, casa 5, Distrito 3',
+habitaciones: 1,
+costo: 450000,
+smoke: false,
+pets: false
+},
+{
+nombre: 'Espectacular casa remodelada',
+id: '23773',
+descripcion: 'Casa en condominio privado, recién remodelada',
+ubicacion: 'Avenida apartada 782, casa 12, Distrito 13',
+habitaciones: 4,
+costo: 4000000,
+smoke: true,
+pets: true
+},
+]
---
+++ Modulo3_Javascript_para_la_web/desafio_4/assets/js/script_venta.js
@@ -0,0 +1,52 @@
+const propiedades_venta = [
+{
+nombre: 'Mágica casa al borde del lago',
+id: '157',
+descripcion: 'Hermosa casa con vista y ambiente inigualable al borde del lago',
+ubicacion: 'Camino del lago 12, Distrito 4',
+habitaciones: 20,
+costo: 15000000000 ,
+smoke: false,
+pets: true
+},
+{
+nombre: 'Casa en el cerro',
+id: '9528',
+descripcion: 'Casa en sector privado, tranquilo y seguro',
+ubicacion: 'Calle del cerro 453, Distrito 6',
+habitaciones: 5,
+costo: 800000000 , 
+smoke: false,
+pets: true
+},
+{
+nombre: 'Home studio en barrio universitario',
+id: '12288',
+descripcion: 'Ideal para estudiantes, home studio con cocina equipada',
+ubicacion: 'Avenida del universitario 6575, Distrito 2',
+habitaciones: 1,
+costo: 120000000,
+smoke: false,
+pets: false
+},
+{
+nombre: 'Casa con amplio jardín',
+id: '12900',
+descripcion: 'Pintoresca casa en sector tranquilo, cercano a la playa con amplio jardín',
+ubicacion: 'Sendero a la playa 3278, Distrito 8',
+habitaciones: 3,
+costo: 200000000,
+smoke: true,
+pets: true
+},
+{
+nombre: 'Departamento nuevo en icónico barrio',
+id: '14696',
+descripcion: 'Amplio departamento con terraza en sector familiar y seguro',
+ubicacion: 'Av. La familia 5689, Distrito 9',
+habitaciones: 4,
+costo: 500000000,
+smoke: false,
+pets: false
+},
+]
