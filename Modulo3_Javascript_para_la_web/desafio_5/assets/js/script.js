d4a416727cd2cbc2cf372710c17c15531679ba1f
Camila Hurtado <camilahurtadoc@gmail.com>
2025-05-24 13:35:27 -0400

    importar(pegar) trabajo realizado en guia (actividad 9)

---
+++ Modulo3_Javascript_para_la_web/desafio_5/assets/js/script.js
@@ -0,0 +1,56 @@
+const inputTareas = document.querySelector("#tareaInput");
+const btnAgregar = document.querySelector("#btnTarea");
+const listaTareas = document.querySelector("#listaInvitados");
+const cuentaTareas = document.querySelector("#cuentaTareas");
+
+const tareas = [{id: 1, descripcion: "Comprar pan"}, {id: 2, descripcion: "Bañar al perro"}, {id: 3, descripcion: "Terminar desafío 5"}];
+renderTareas(tareas)
+
+
+btnAgregar.addEventListener("click", () => {
+    const nuevaTarea = inputTareas.value;
+    tareas.push({id: Date.now(), descripcion:nuevaTarea});
+    inputTareas.value = "";
+
+    renderTareas(tareas)
+})
+
+
+function agregarItemsALista(arreglo) {
+    let htmlGeneral = "";
+    for (let item of arreglo){
+        htmlGeneral += `
+        <li>${item.descripcion} <button onclick="borrarTarea(${item.id})">Eliminar</button></li>
+        `
+    }
+    return htmlGeneral;
+}
+
+function borrarTarea(id) {
+    const index = tareas.findIndex(tarea => tarea.id === id)
+
+    if (index !== -1) {
+        tareas.splice(index, 1);
+    }
+    
+    renderTareas(tareas)
+}
+
+function renderTareas(arreglo) {
+    listaTareas.innerHTML = agregarItemsALista(arreglo);
+    cuentaTareas.innerHTML = `<p>Cantidad de tareas: ${arreglo.length}</p>`
+}
+
+const buscarInput = document.querySelector("#buscarInput");
+const btnBuscar = document.querySelector("#btnBuscar");
+
+btnBuscar.addEventListener("click", () => {
+    const frase = buscarInput.value;
+    console.log(frase)
+    const tareasFiltradas = tareas.filter( 
+        tarea => tarea.descripcion.includes(frase)
+    
+     );
+
+    renderTareas(tareasFiltradas)
+})
