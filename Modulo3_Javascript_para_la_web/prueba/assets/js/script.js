a69872d29ec5f71fd77bebf756818c6352b2c172
Camila Hurtado <camilahurtadoc@gmail.com>
2025-05-31 10:06:30 -0400

    agrega funcion parseo de datos para chart dentro de función asincrónica y elimina uf

--- Modulo3_Javascript_para_la_web/prueba/assets/js/script.js
+++ Modulo3_Javascript_para_la_web/prueba/assets/js/script.js
@@ -6,6 +6,8 @@ btnBuscar.addEventListener('click', () => {
     const inputCLP = document.querySelector("#inputCLP").value;
     const monedaUsuario = document.querySelector("#monedaUsuario").value;
 
+    parsearMoneda(monedaUsuario, inputCLP);
+    renderChartyResultado(monedaUsuario, inputCLP);
 
 })
 
@@ -21,18 +23,46 @@ async function getMonedaEscogidaUsuario(monedaUsuario) {
 
 //traer conversión hoy (async function) y (parsear info)
 //parsearDatosChart()
+async function parsearMoneda(monedaUsuario, inputCLP) {
+    const moneda = await getMonedaEscogidaUsuario(monedaUsuario);
+    const valorPesos = moneda.serie[0].valor;
+    const pResultado = document.querySelector("#resultado");
+    pResultado.innerHTML = `Resultado: $${(inputCLP/valorPesos).toFixed(2)}`;
+    
+    let labels = [];
+    let fecha = "";
+    let counter = 0;
+    for (i = 9; i > -1; i --) {
+        fecha = (moneda.serie[i].fecha).substr(0, 10);
+        labels[counter] = fecha;
+        counter += 1;
+    }
+
+    let ejeY = [];
+    let valorDia = 0;
+    counter = 0;
+    for (i = 9; i > 0; i --) {
+        valorDia = moneda.serie[i].valor;
+        ejeY[counter] = valorDia;
+        counter += 1;
+    }
+
+    const datasets = [{
+        label: "Historial últimos 10 días",
+        borderColor: "red",
+        backgroundColor: "blue",
+        data: ejeY
+    }]
+    
+    return {labels, datasets}
+}
+
 //renderChartyResultado
 //      => render queryselector("#resultado") y innerhtml = `${valor}`
 //      => render chart
 
-
-
-async function getMonedasHoy() {
-    const res = await fetch("https://mindicador.cl/api/");
-    const monedasHoy = res.json();
-    return monedasHoy;
-}
-
-async function parsearMonedas() {
+async function renderChartyResultado(monedaUsuario, inputCLP) {
     
 }
+
+
