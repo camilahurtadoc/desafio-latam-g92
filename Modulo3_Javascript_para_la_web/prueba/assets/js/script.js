6a6bb90db463331953fa4e5dcde245864a7d233e
Camila Hurtado <camilahurtadoc@gmail.com>
2025-05-31 22:31:25 -0400

    implementación .then para Promise, try y catch // implementación condicional para inputs

--- Modulo3_Javascript_para_la_web/prueba/assets/js/script.js
+++ Modulo3_Javascript_para_la_web/prueba/assets/js/script.js
@@ -6,18 +6,22 @@ let chartInUse;
 btnBuscar.addEventListener('click', async () => {
     const inputCLP = document.querySelector("#inputCLP").value;
     const monedaUsuario = document.querySelector("#monedaUsuario").value;
-    const tooltiptext = document.querySelector(".tooltiptext");
+    const tooltiptextPesos = document.querySelector("#tooltiptextPesos");
+    const tooltiptextMoneda = document.querySelector("#tooltiptextMoneda");
 
     // Asegura que usuario escoge una moneda
-    if (monedaUsuario == "seleccion") {
-        tooltiptext.style.visibility = "visible";
+    if (inputCLP <= 0) {
+        tooltiptextPesos.style.visibility = "visible";
+    } else if (monedaUsuario == "seleccion") {
+        tooltiptextMoneda.style.visibility = "visible";
     } else {
-        tooltiptext.style.visibility = "hidden";
+        tooltiptextPesos.style.visibility = "hidden";
+        tooltiptextMoneda.style.visibility = "hidden";
         // Permite un chart nuevo si ya hay uno creado
         if (chartInUse != undefined) {
             chartInUse.destroy();
         }
-        // Renderización de chart
+        // Renderización de datos dentro de un try / catch
         chartInUse = await renderChart(monedaUsuario, inputCLP, chartInUse)
     }
 })
@@ -25,83 +29,108 @@ btnBuscar.addEventListener('click', async () => {
 
 // GET moneda escogida por usuario desde API
 async function getMonedaEscogidaUsuario(monedaUsuario) {
-    const res = await fetch(`https://mindicador.cl/api/${monedaUsuario}`);
-    const moneda = await res.json();
-    return moneda;
+    try {
+        const res = await fetch(`https://mindicador.cl/api/${monedaUsuario}`)
+            .then((response) => {
+
+                if (!response.ok) {
+                    throw new Error(`HTTP Error Status ${response.status}`);
+                }
+                return response;
+            })
+        const moneda = await res.json();
+        return moneda;
+    } catch (e) {
+        console.log("Error en get: ", e)
+        const boxConversor = document.querySelector(".boxConversor")
+        boxConversor.innerHTML = `
+                    <h2>Error al buscar información</h2>
+                    <p style="text-align: center">${e}</p>
+                    `
+    }
 }
 
 
 // Parseo de datos desde API
 async function parsearMoneda(monedaUsuario, inputCLP) {
-    const moneda = await getMonedaEscogidaUsuario(monedaUsuario);
-    const valorPesos = moneda.serie[0].valor;
-    mostrarResultado(inputCLP, valorPesos);
-    
-    // Parseo de datos para chart
-    let labels = [];
-    let fecha = "";
-    let counter = 0;
-    for (i = 9; i > -1; i --) {
-        fecha = (moneda.serie[i].fecha).substr(0, 10);
-        labels[counter] = fecha;
-        counter += 1;
-    }
+    try {
+        const moneda = await getMonedaEscogidaUsuario(monedaUsuario);
+        const valorPesos = moneda.serie[0].valor;
+        mostrarResultado(inputCLP, valorPesos);
 
-    let ejeY = [];
-    let valorDia = 0;
-    counter = 0;
-    for (i = 9; i > -1; i --) {
-        valorDia = moneda.serie[i].valor;
-        ejeY[counter] = valorDia;
-        counter += 1;
-    }
+        // Parseo de datos para chart
+        let labels = [];
+        let fecha = "";
+        let counter = 0;
+        for (i = 9; i > -1; i--) {
+            fecha = (moneda.serie[i].fecha).substr(0, 10);
+            labels[counter] = fecha;
+            counter += 1;
+        }
 
-    const datasets = [{
-        label: "Historial últimos 10 días",
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgb(165, 28, 58)",
-        data: ejeY
-    }]
-    
-    return {labels, datasets}
+        let ejeY = [];
+        let valorDia = 0;
+        counter = 0;
+        for (i = 9; i > -1; i--) {
+            valorDia = moneda.serie[i].valor;
+            ejeY[counter] = valorDia;
+            counter += 1;
+        }
+
+        const datasets = [{
+            label: "Historial últimos 10 días",
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgb(165, 28, 58)",
+            data: ejeY
+        }]
+
+        return { labels, datasets }
+    } catch (e) {
+        console.log("Error en parsearMonedas():", e);
+        throw new Error(e)
+    }
 }
 
 
 // Modifica DOM y muestra resultado de moneda seleccionada por usuario
 function mostrarResultado(inputCLP, valorPesos) {
     const pResultado = document.querySelector("#resultado");
-    pResultado.innerHTML = `Resultado: $${(inputCLP/valorPesos).toFixed(2)}`;
+    pResultado.innerHTML = `Resultado: $${(inputCLP / valorPesos).toFixed(2)}`;
 }
 
 
 // Parseo de datos para chart
 async function parsearChart(monedaUsuario, inputCLP) {
-    const data = await parsearMoneda(monedaUsuario, inputCLP);
-    const plugin = {
-        id: 'customCanvasBackgroundColor',
-        beforeDraw: (chart, args, options) => {
-            const {ctx} = chart;
-            ctx.save();
-            ctx.globalCompositeOperation = 'destination-over';
-            ctx.fillStyle = options.color || '#ededed';
-            ctx.fillRect(0, 0, chart.width, chart.height);
-            ctx.restore();
+    try {
+        const data = await parsearMoneda(monedaUsuario, inputCLP);
+        const plugin = {
+            id: 'customCanvasBackgroundColor',
+            beforeDraw: (chart, args, options) => {
+                const { ctx } = chart;
+                ctx.save();
+                ctx.globalCompositeOperation = 'destination-over';
+                ctx.fillStyle = options.color || '#ededed';
+                ctx.fillRect(0, 0, chart.width, chart.height);
+                ctx.restore();
             }
-    };
-    const config = {
-        type: "line",
-        data,
-        options: {
-            plugins: {
-                customCanvasBackgroundColor: {
-                    color: "#ededed"
+        };
+        const config = {
+            type: "line",
+            data,
+            options: {
+                plugins: {
+                    customCanvasBackgroundColor: {
+                        color: "#ededed"
+                    }
                 }
-            }
-        },
-        plugins: [plugin],
+            },
+            plugins: [plugin],
+        }
+
+        return config;
+    } catch (e) {
+        console.log("error en parsearChart():", e)
     }
-        
-    return config;
 }
 
 
@@ -111,8 +140,6 @@ async function renderChart(monedaUsuario, inputCLP, chartInUse) {
 
     const myChart = document.querySelector("#myChart");
     chartInUse = new Chart(myChart, config);
-    
+
     return chartInUse;
 }
-
-
