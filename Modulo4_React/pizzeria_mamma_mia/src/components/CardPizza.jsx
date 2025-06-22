64dc1fe9701f528dba0b3bfc7a93e534d294452d
Camila Hurtado <camilahurtadoc@gmail.com>
2025-06-21 21:01:58 -0400

    agrega cardPizza

--- Modulo4_React/pizzeria_mamma_mia/src/components/CardPizza.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/CardPizza.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
-function Cardpizza() {
+
+function CardPizza({ name, price, ingredients, img }) {
+    let items = "";
+    function displayIngredients(ingredients) {
+        for (let item of ingredients) {
+            items += item + ", "
+        }
+        return items.slice(0, -2);
+    }
+
   return (
-    <div>Cardpizza</div>
+    <Card style={{ width: '20rem' }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item style={{fontSize: '0.8em'}}>
+            <h6>Ingredientes</h6>
+            {displayIngredients(ingredients)}
+        </ListGroup.Item>
+        <ListGroup.Item style={{textAlign: 'center'}}><h5>Precio: ${price}</h5></ListGroup.Item>
+      </ListGroup>
+      <Card.Body className="d-flex justify-content-around" >
+        <Button variant="light">Ver más</Button>
+        <Button variant="dark" >Añadir</Button>
+
+      </Card.Body>
+    </Card>
   )
 }
 
-export default Cardpizza
+export default CardPizza
