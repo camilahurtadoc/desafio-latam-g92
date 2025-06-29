c24fd58c20b9067594ca62d5460ba62478549044
Camila Hurtado <camilahurtadoc@gmail.com>
2025-06-28 20:32:11 -0400

    agrega mensajes de error y condicionales

--- Modulo4_React/pizzeria_mamma_mia/src/App.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
-import Register from './components/Register'
+import Register from './components/Register/Register'
 
 function App() {
 
--- Modulo4_React/pizzeria_mamma_mia/src/components/Register/Register.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Register/Register.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import './Register.css'
 
 const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errorMandatory, setErrorMandatory] = useState(false);
+    const [errorPasswordLength, setErrorPasswordLength] = useState(false)
+    const [errorPasswordConfirmation, setErrorPasswordConfirmation] = useState(false)
+
+    const [eye, setEye] = useState(faEyeSlash)
+    const [type, setType] = useState("password")
+
+    const seePassword = () => {
+        if (type === "password") {
+            setEye(faEye)
+            setType("text")
+        } else {
+            setEye(faEyeSlash)
+            setType("password")
+        }
+    }
 
     const validate = (event) => {
         event.preventDefault();
@@ -16,6 +35,24 @@ const Register = () => {
             return
         }
         setErrorMandatory(false)
+
+        if (password.length < 6) {
+            setErrorPasswordLength(true)
+            return
+        }
+        setErrorPasswordLength(false)
+
+        if (password != confirmPassword) {
+            setErrorPasswordConfirmation(true)
+            return
+        }
+        setErrorPasswordConfirmation(false)
+
+        setEmail("")
+        setPassword("")
+        setConfirmPassword("")
+        setEye(faEyeSlash)
+        setType("password")
     }
 
 
@@ -23,7 +60,7 @@ const Register = () => {
 
     return (
         <div className='mx-auto p-2' style={{ width: "450px" }}>
-            <h2>Regístrate</h2>
+            <h2 className='text-center'>Regístrate</h2>
             <Form className='p-3' onSubmit={validate}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
@@ -37,16 +74,20 @@ const Register = () => {
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3 position-relative" controlId="formBasicPassword">
                     <Form.Label>Contraseña</Form.Label>
                     <Form.Control
-                        type="password"
+                        type={type}
                         placeholder="Contraseña"
                         onChange={event => setPassword(event.target.value)}
                         value={password} />
+                    <span className='span-icon position-absolute end-0 translate-middle-x'
+                    onClick={seePassword}>
+                        <FontAwesomeIcon icon={eye}/>
+                    </span>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3 position-relative" controlId="formBasicPassword">
                     <Form.Label>Confirmar contraseña</Form.Label>
                     <Form.Control
                         type="password"
@@ -55,9 +96,15 @@ const Register = () => {
                         value={confirmPassword} />
                 </Form.Group>
 
-                <div className='d-flex justify-content-center'>
+                <div className='d-flex flex-column mb-3 align-items-center'>
                     {
-                        errorMandatory ? <p className='error-mandatory'>Todos los campos son obligatorios.</p> : null
+                        errorMandatory ? <p className='error-mandatory text-light bg-danger'>Todos los campos son obligatorios.</p> : null
+                    }
+                    {
+                        errorPasswordLength ? <p className='error-mandatory text-light bg-danger'>La contraseña debe tener más de 6 caracteres.</p> : null
+                    }
+                    {
+                        errorPasswordConfirmation ? <p className='error-mandatory text-light bg-danger'>La contraseña y confirmación de contraseña deben ser iguales.</p> : null
                     }
                     <Button variant="primary" type="submit">
                         Crear cuenta
