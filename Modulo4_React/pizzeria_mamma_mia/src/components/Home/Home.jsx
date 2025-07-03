0d87411c6a05a6c2d772ce7a83cd2efb9601f839
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-03 16:21:09 -0400

    importa pizzas.js para los distintos CardPizza en Home

--- Modulo4_React/pizzeria_mamma_mia/src/components/Home/Home.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Home/Home.jsx
@@ -1,8 +1,13 @@
 import CardPizza from '../Cardpizza/CardPizza'
 import Header from '../Header/Header'
+import { pizzas } from '../../assets/js/pizzas'
+import { useState } from 'react'
 
 
 function Home() {
+
+    const [listaPizzas, setListaPizzas] = useState(pizzas)
+
     return (
         <>
             <Header
@@ -10,25 +15,17 @@ function Home() {
                 description="¡Tenemos las mejores pizzas que podrás encontrar!" />
 
             <div style={{display: 'flex', justifyContent: 'center', gap: '2em', paddingTop: '2em'}}>
-            <CardPizza 
-                name="Napolitana"
-                price={5950}
-                ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
-                />
-            <CardPizza
-                name="Española"
-                price={6950}
-                ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"
-                />
-            <CardPizza
-                name="Pepperoni"
-                price={6950}
-                ingredients={["mozzarella", "pepperoni", "orégano"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
-                />
-                </div>
+                {
+                    listaPizzas.map(item => (
+                        <CardPizza 
+                            name={item.name}
+                            price={item.price}
+                            ingredients={item.ingredients}
+                            img={item.img}                        
+                        />
+                    ))
+                }
+            </div>
 
         </>
     )
