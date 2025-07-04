3fa3fe8fad51134ac324e2015b579c53051ad89b
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-04 17:00:17 -0400

    agrega logica total valor del pedido en  Cart

--- Modulo4_React/pizzeria_mamma_mia/src/components/Cart/Cart.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { pizzaCart } from "../../assets/js/pizzas"
 import Button from 'react-bootstrap/Button';
 import './Cart.css'
+import { useState } from "react";
 
 const Cart = () => {
 
@@ -10,20 +11,44 @@ const Cart = () => {
     return firstLetterCapt + otherLetters
   }
 
+    let startingTotal = 0
+    const startingTotalArray = pizzaCart.map(pizza => startingTotal += pizza.count*pizza.price)
+    
+
+    const [counterPizza, setCounterPizza] = useState(1)
+    const [total, settotal] = useState(startingTotal)
+
+    const minusPizza = (pizza) => {
+        if (counterPizza - 1 < 0) {
+            return
+        }
+        setCounterPizza(counterPizza -1)
+        settotal(total - pizza.price)
+    }
+
+    const plusPizza = (pizza) => {
+        setCounterPizza(counterPizza + 1)
+        console.log(total, pizza.price)
+        settotal(total + pizza.price)
+    }
+
+
   return (
     <div className="m-2">
         <h3>Detalles del pedido:</h3>
         {pizzaCart.map(item => (
-            <div key={item.id} className="d-flex align-items-center m-4 gap-4">
+            
+
+            <div key={item.id} className="d-flex align-items-center m-4 ps-5 gap-4">
                 <img src={item.img} className="pizza-cart"/>
                 <h4 className="pizza-name">{capitalizeFirstLetter(item.name)}</h4>
                 <span className="fw-bold">${item.price.toLocaleString("es-ES", { useGrouping: true })}</span>
-                <Button variant="danger">-</Button>
-                <span>{item.count}</span>
-                <Button variant="success">+</Button>
+                <Button variant="danger" onClick={() => minusPizza(item)}>-</Button>
+                <span>{counterPizza}</span>
+                <Button variant="success" onClick={() => plusPizza(item)}>+</Button>
             </div>
         ))}
-        <h2>Total: ${}</h2>
+        <h2>Total: ${total.toLocaleString("es-ES", { useGrouping: true })}</h2>
         <Button>Pagar</Button>
     </div>
   )
