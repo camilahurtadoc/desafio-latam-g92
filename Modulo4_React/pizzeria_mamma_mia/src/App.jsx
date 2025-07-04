a39096d5bd9bed0fda8e656afbcd461ef49fcfb7
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-04 19:58:22 -0400

    implementa grid para display de CardPizza en Home.jsx

--- Modulo4_React/pizzeria_mamma_mia/src/App.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <Navbar />
       <Cart />
-      {/* <Home /> */}
+      <Home />
       {/* <Register /> */}
       {/* <Login /> */}
       <Footer />
--- Modulo4_React/pizzeria_mamma_mia/src/components/Home/Home.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Home/Home.jsx
@@ -2,6 +2,9 @@ import CardPizza from '../Cardpizza/CardPizza'
 import Header from '../Header/Header'
 import { pizzas } from '../../assets/js/pizzas'
 import { useState } from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function Home() {
@@ -14,19 +17,23 @@ function Home() {
                 title="¡Pizzería Mamma Mia!"
                 description="¡Tenemos las mejores pizzas que podrás encontrar!" />
 
-            <div style={{display: 'flex', justifyContent: 'center', gap: '2em', paddingTop: '2em'}}>
+            <Container className='mt-5'>
+                <Row md={3}>
                 {
                     listaPizzas.map(item => (
-                        <CardPizza 
-                            key={item.id}
-                            name={item.name}
-                            price={item.price}
-                            ingredients={item.ingredients}
-                            img={item.img}
-                        />
+                            <Col >
+                                <CardPizza
+                                    key={item.id}
+                                    name={item.name}
+                                    price={item.price}
+                                    ingredients={item.ingredients}
+                                    img={item.img}
+                                />
+                            </Col>
                     ))
                 }
-            </div>
+                </Row>
+            </Container>
 
         </>
     )
