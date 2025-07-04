2a1f96d1bfc644285a1802acb278946c151d800e
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-04 18:07:54 -0400

    implementa logica para borrar pizza del carrito cuando cantidad es 0

--- Modulo4_React/pizzeria_mamma_mia/src/components/Cart/Cart.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Cart/Cart.jsx
@@ -6,14 +6,14 @@ import { useState } from "react";
 const Cart = () => {
 
     const capitalizeFirstLetter = (word) => {
-    const firstLetterCapt = word.charAt(0).toUpperCase()
-    const otherLetters = word.slice(1)
-    return firstLetterCapt + otherLetters
-  }
+        const firstLetterCapt = word.charAt(0).toUpperCase()
+        const otherLetters = word.slice(1)
+        return firstLetterCapt + otherLetters
+    }
 
     let startingTotal = 0
-    const startingTotalArray = pizzaCart.map(pizza => startingTotal += pizza.count*pizza.price)
-    
+    const startingTotalArray = pizzaCart.map(pizza => startingTotal += pizza.count * pizza.price)
+
 
     const [counterPizza, setCounterPizza] = useState(1)
     const [total, settotal] = useState(startingTotal)
@@ -22,36 +22,63 @@ const Cart = () => {
         if (counterPizza - 1 < 0) {
             return
         }
-        setCounterPizza(counterPizza -1)
+        setCounterPizza(counterPizza - 1)
         settotal(total - pizza.price)
     }
 
     const plusPizza = (pizza) => {
         setCounterPizza(counterPizza + 1)
-        console.log(total, pizza.price)
         settotal(total + pizza.price)
     }
 
+    const pizzaCartDisplay = pizzaCart.map(item =>
 
-  return (
-    <div className="m-2">
-        <h3>Detalles del pedido:</h3>
-        {pizzaCart.map(item => (
+
+
+
+
+        <div key={item.id} className="d-flex align-items-center m-4 ps-5 gap-4">
+            <img src={item.img} className="pizza-cart" />
+            <h4 className="pizza-name">{capitalizeFirstLetter(item.name)}</h4>
+            <span className="fw-bold">${item.price.toLocaleString("es-ES", { useGrouping: true })}</span>
+            <Button variant="danger" onClick={() => minusPizza(item)}>-</Button>
+            <span>{counterPizza}</span>
+            <Button variant="success" onClick={() => plusPizza(item)}>+</Button>
+        </div>
+
+
+
+    )
+
+    return (
+        <div className="m-2">
+            <h3>Detalles del pedido:</h3>
+            
+                {pizzaCart.map(item => (
+                    <div key={item.id}>
+                        {
+                            counterPizza > 0 ? (
+                                <div className="d-flex align-items-center m-4 ps-5 gap-4">
+
+                                    <img src={item.img} className="pizza-cart" />
+                                    <h4 className="pizza-name">{capitalizeFirstLetter(item.name)}</h4>
+                                    <span className="fw-bold">${item.price.toLocaleString("es-ES", { useGrouping: true })}</span>
+                                    <Button variant="danger" onClick={() => minusPizza(item)}>-</Button>
+                                    <span>{counterPizza}</span>
+                                    <Button variant="success" onClick={() => plusPizza(item)}>+</Button>
+
+                                </div>
+                            ) : null
+                        }
+
+                    </div>
+                ))}
             
 
-            <div key={item.id} className="d-flex align-items-center m-4 ps-5 gap-4">
-                <img src={item.img} className="pizza-cart"/>
-                <h4 className="pizza-name">{capitalizeFirstLetter(item.name)}</h4>
-                <span className="fw-bold">${item.price.toLocaleString("es-ES", { useGrouping: true })}</span>
-                <Button variant="danger" onClick={() => minusPizza(item)}>-</Button>
-                <span>{counterPizza}</span>
-                <Button variant="success" onClick={() => plusPizza(item)}>+</Button>
-            </div>
-        ))}
-        <h2>Total: ${total.toLocaleString("es-ES", { useGrouping: true })}</h2>
-        <Button>Pagar</Button>
-    </div>
-  )
+            <h2>Total: ${total.toLocaleString("es-ES", { useGrouping: true })}</h2>
+            <Button>Pagar</Button>
+        </div>
+    )
 }
 
 export default Cart
