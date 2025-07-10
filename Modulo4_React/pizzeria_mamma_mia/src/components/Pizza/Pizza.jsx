56c452448051bed567c17e35cbceeb096b554925
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-10 13:18:02 -0400

    consumo de API para componente Pizza

--- Modulo4_React/pizzeria_mamma_mia/src/components/Pizza/Pizza.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Pizza/Pizza.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const Pizza = () => {
+
+    const [pizza, setPizza] = useState({})
+
+    async function getPizza() {
+  try {
+    const { data } = await axios.get('http://localhost:5000/api/pizzas/p001');
+    setPizza(data)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+useEffect(() => {
+    getPizza()
+}, [])
+
   return (
     <div>
         <h2>Pizza</h2>
