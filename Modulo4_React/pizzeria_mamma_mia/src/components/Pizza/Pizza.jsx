bc0fb8d3cd0bc222e02d89ff2e532812f048e983
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-10 13:36:44 -0400

    muestra datos de API en pg web

--- Modulo4_React/pizzeria_mamma_mia/src/components/Pizza/Pizza.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Pizza/Pizza.jsx
@@ -5,24 +5,40 @@ const Pizza = () => {
 
     const [pizza, setPizza] = useState({})
 
+    const capitalizeFirstLetter = (word) => {
+        const firstLetterCapt = word.charAt(0).toUpperCase()
+        const otherLetters = word.slice(1)
+        return firstLetterCapt + otherLetters
+    }
+
     async function getPizza() {
-  try {
-    const { data } = await axios.get('http://localhost:5000/api/pizzas/p001');
-    setPizza(data)
-  } catch (error) {
-    console.error(error);
-  }
-}
+        try {
+            const { data } = await axios.get('http://localhost:5000/api/pizzas/p001');
+            setPizza(data)
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-useEffect(() => {
-    getPizza()
-}, [])
+    useEffect(() => {
+        getPizza()
+    }, [])
 
-  return (
-    <div>
-        <h2>Pizza</h2>
-    </div>
-  )
+    return (
+        <div>
+            <h2>{pizza.name && capitalizeFirstLetter(pizza.name)}</h2>
+            <p>${pizza.price && pizza.price.toLocaleString("es-ES", { useGrouping: true })}</p>
+            <ul>
+                {
+                    pizza.ingredients && pizza.ingredients.map((ingr, index) => (
+                        <li key={index}>{ingr}</li>
+                    ))
+                }
+            </ul>
+            <img src={pizza.img} />
+            <p>{pizza.desc}</p>
+        </div>
+    )
 }
 
 export default Pizza
