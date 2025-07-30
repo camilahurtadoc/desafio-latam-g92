b2821800a4ee13b5aae9ae94a2ed1c51801ae9d4
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-30 17:13:09 -0400

    agrega estilo img NotFound

--- Modulo4_React/pizzeria_mamma_mia/src/components/NotFound/NotFound.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/NotFound/NotFound.jsx
@@ -2,10 +2,10 @@ import  ninjaTurtles from '../../assets/img/notfound.jpg'
 
 const NotFound = () => {
   return (
-    <div className="d-flex flex-column align-items-center gap-2 mt-5">
+    <div className="d-flex flex-column align-items-center gap-2 mt-3">
         <h2>¿Pizzas?</h2>
         <h2>¡Acá solo comemos pizas!</h2>
-        <img src={ninjaTurtles} className='w-50' alt='tortugas ninja comiendo pizza'/>
+        <img src={ninjaTurtles} className='rounded w-50' alt='tortugas ninja comiendo pizza' />
         <h3 className='my-5'>La página que estás buscando no está acá.</h3>
     </div>
   )
