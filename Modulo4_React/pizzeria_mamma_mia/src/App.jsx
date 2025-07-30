cef053662fc3e19c19a8c0e28cce49635e34e1e7
Camila Hurtado <camilahurtadoc@gmail.com>
2025-07-30 17:20:02 -0400

    agrega ruta /404 a App

--- Modulo4_React/pizzeria_mamma_mia/src/App.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <Route path='/cart' element={<Cart />}/>
         <Route path='/pizza/p001' element={<Pizza />}/>
         <Route path='/profile' element={<Profile />}/>
+        <Route path='/404' element={<NotFound />}/>
         <Route path='/*' element={<NotFound />}/>
       </Routes>
       <Footer />
