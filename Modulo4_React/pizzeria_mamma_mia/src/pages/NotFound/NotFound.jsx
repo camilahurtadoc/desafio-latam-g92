317ed3fb8420281da563c7ce17a2497be452eb82
Camila Hurtado <camilahurtadoc@gmail.com>
2025-08-04 11:25:54 -0400

    corrección ortografía

--- Modulo4_React/pizzeria_mamma_mia/src/pages/NotFound/NotFound.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/pages/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className="d-flex flex-column align-items-center gap-2 mt-3">
         <h2>¿Pizzas?</h2>
-        <h2>¡Acá solo comemos pizas!</h2>
+        <h2>¡Acá solo comemos pizzas!</h2>
         <img src={ninjaTurtles} className='rounded w-50' alt='tortugas ninja comiendo pizza' />
         <h3 className='my-5'>La página que estás buscando no está acá.</h3>
     </div>
