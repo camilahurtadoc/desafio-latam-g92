51086c721b16c2081e77888efc8db21ef219b22f
Camila Hurtado <camilahurtadoc@gmail.com>
2025-08-06 17:11:40 -0400

    agrega Context de Card

---
+++ Modulo4_React/pizzeria_mamma_mia/src/context/CartContext.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+
+export const CartContext = useContext();
+
+const ContextProvider = ({ children }) => {
+    const [price, setPrice] = useState(0)
+
+    return (
+        <ContextProvider>
+            {children}
+        </ContextProvider>
+    )
+}
