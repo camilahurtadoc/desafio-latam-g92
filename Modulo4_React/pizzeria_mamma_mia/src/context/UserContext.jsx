ff972bafb6529b847ae2bdd20af468209079891b
Camila Hurtado <camilahurtadoc@gmail.com>
2025-08-14 17:39:05 -0400

    crea UserContext con State token y metodo logout

---
+++ Modulo4_React/pizzeria_mamma_mia/src/context/UserContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+    const [token, setToken] = useState(true)
+
+    const logout = () => {
+        setToken(false)
+    }
+    return (
+        <UserContext.Provider value={{ token, setToken, logout }}>
+            { children }
+        </UserContext.Provider>
+    )
+}
+
+export default UserProvider;
