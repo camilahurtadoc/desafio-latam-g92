83b8e4fbac808700c5f6cb90d754636e1e6feb80
Camila Hurtado <camilahurtadoc@gmail.com>
2025-08-14 19:54:07 -0400

    implementa isActive y NavLink a Navbar

--- Modulo4_React/pizzeria_mamma_mia/src/components/Navbar/Navbar.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/components/Navbar/Navbar.jsx
@@ -5,15 +5,18 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom'
+import {  NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping, faPizzaSlice, faUser, faUserPlus, faDoorOpen } from '@fortawesome/free-solid-svg-icons'
 import { UserContext } from '../../context/UserContext';
 
 
 function NavbarTop() {
-    const {total} = useContext(CartContext);
-    const {token, setToken, logout } = useContext(UserContext)
+    const { total } = useContext(CartContext);
+    const { token, setToken, logout } = useContext(UserContext)
+
+    const setActiveClass = ({ isActive, isPending }) => (isPending ? "pending nav-link" : isActive ? "active nav-link" : "nav-link")
+
 
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
@@ -22,36 +25,33 @@ function NavbarTop() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/" className="nav-link mx-1"><FontAwesomeIcon icon={faPizzaSlice} /> Home</Link>
+                        <NavLink to="/" className={setActiveClass}><FontAwesomeIcon icon={faPizzaSlice} /> Home</NavLink>
                         {token ? (
                             <>
-                                <Link to="/profile" className="nav-link mx-1"><FontAwesomeIcon icon={faUser} /> Profile</Link>
-                                <Link to="/" className="nav-link mx-1" onClick={logout}><FontAwesomeIcon icon={faDoorOpen} /> Logout</Link>                            </>
+                                <NavLink to="/profile" className={setActiveClass}><FontAwesomeIcon icon={faUser} /> Profile</NavLink>
+                                <NavLink to="/" onClick={logout} className={setActiveClass}><FontAwesomeIcon icon={faDoorOpen} /> Logout</NavLink>
+                            </>
 
                         ) : (
                             <>
-                                <Link to="/login" className="nav-link mx-1"><FontAwesomeIcon icon={faUser} /> Login</Link>
-                                <Link to="/register" className="nav-link mx-1"><FontAwesomeIcon icon={faUserPlus} /> Register</Link>                                
-                                {/* Versión opcional para Link, manteniendo el NavLink de bootstrap*/}
-                                {/* <Link to="/ruta-deseada" className="text-decoration-none">
-                                    <Nav.Link as="span">Texto del enlace</Nav.Link>
-                                */}
+                                <NavLink to="/login" className={setActiveClass}><FontAwesomeIcon icon={faUser} /> Login</NavLink>
+                                <NavLink to="/register" className={setActiveClass}><FontAwesomeIcon icon={faUserPlus} /> Register</NavLink>
                             </>
                         )}
                         {/* <NavDropdown title="Productos" id="collapsible-nav-dropdown" className="ms-2">
                             <NavDropdown.Item href="#action/3.1">Pizzas Tradicionales</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                Pizzas Vegetarianas
+                            Pizzas Vegetarianas
                             </NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Promociones</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
-                                Postres
+                            Postres
                             </NavDropdown.Item>
-                        </NavDropdown> */}
+                            </NavDropdown> */}
                     </Nav>
                     <Nav>
-                        <Link to="/cart" className="nav-link mx-1"><FontAwesomeIcon icon={faCartShopping} /> Total: $ {total.toLocaleString("es-ES", { useGrouping: true })}</Link>
+                        <NavLink to="/cart" className={setActiveClass}><FontAwesomeIcon icon={faCartShopping} /> Total: $ {total.toLocaleString("es-ES", { useGrouping: true })}</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
