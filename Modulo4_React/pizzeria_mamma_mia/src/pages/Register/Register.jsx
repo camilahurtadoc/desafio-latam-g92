851a7a7dca81c4ccb2f813b4fc2c707deb329c20
Camila Hurtado <camilahurtadoc@gmail.com>
2025-08-20 16:49:46 -0400

    elimina codigo redundante

--- Modulo4_React/pizzeria_mamma_mia/src/pages/Register/Register.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/pages/Register/Register.jsx
@@ -18,66 +18,6 @@ const Register = () => {
         confirmPassword, setConfirmPassword,
         handleSubmitRegister } = useContext(UserContext)
 
-    // const [email, setEmail] = useState("");
-    // const [password, setPassword] = useState("");
-    // const [confirmPassword, setConfirmPassword] = useState("");
-
-    // const [eye, setEye] = useState(faEyeSlash)
-    // const [type, setType] = useState("password")
-
-    // const seePassword = () => {
-    //     if (type === "password") {
-    //         setEye(faEye)
-    //         setType("text")
-    //     } else {
-    //         setEye(faEyeSlash)
-    //         setType("password")
-    //     }
-    // }
-
-    // const handleSubmitRegister = (event) => {
-    //     event.preventDefault();
-
-    //     if (!email.trim() || !password.trim() || !confirmPassword.trim()) {
-    //         Swal.fire({
-    //             title: "Campos Vacíos",
-    //             text: "Todos los campos son obligatorios.",
-    //             icon: "error"
-    //         })
-    //         return
-    //     }
-
-    //     if (password.length < 6) {
-    //         Swal.fire({
-    //             title: "Contraseña",
-    //             text: "La contraseña debe tener al menos 6 caracteres.",
-    //             icon: "error"
-    //         })
-    //         return
-    //     }
-
-    //     if (password != confirmPassword) {
-    //         Swal.fire({
-    //             title: "Contraseña",
-    //             text: "La contraseña y confirmación de contraseña deben ser iguales.",
-    //             icon: "error"
-    //         })
-    //         return
-    //     }
-
-
-    //     Swal.fire({
-    //         title: "Registro Exitoso",
-    //         text: "Datos ingresados correctamente.",
-    //         icon: "success"
-    //     })
-    //     setEmail("")
-    //     setPassword("")
-    //     setConfirmPassword("")
-    //     setEye(faEyeSlash)
-    //     setType("password")
-    // }
-
     return (
         <div className='mx-auto p-2' style={{ width: "450px" }}>
             <h2 className='text-center'>Regístrate</h2>
