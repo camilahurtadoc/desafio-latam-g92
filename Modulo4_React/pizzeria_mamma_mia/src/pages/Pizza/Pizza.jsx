c97394189cc1c4cb26c7242d237d9dba59036fe6
Camila Hurtado <camilahurtadoc@gmail.com>
2025-08-27 15:19:09 -0400

    implementa botón Añadir en Pizza.jsx

--- Modulo4_React/pizzeria_mamma_mia/src/pages/Pizza/Pizza.jsx
+++ Modulo4_React/pizzeria_mamma_mia/src/pages/Pizza/Pizza.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom"
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Button from "react-bootstrap/esm/Button";
 import ListGroup from 'react-bootstrap/ListGroup'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCartPlus, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import './Pizza.css'
 import { AxiosPizzaIdContext } from "../../context/AxiosPizzasIdContext";
 import CapitalizeFirstLetter from "../../components/CapitalizeFirstLetter/CapitalizeFirstLetter";
+import { CartContext } from "../../context/CartContext";
 
 const Pizza = () => {
 
@@ -14,13 +15,27 @@ const Pizza = () => {
 
     const { pizza, getPizza } = useContext(AxiosPizzaIdContext)
 
+    const { total, setTotal, cart, setCart,
+        pizzaCount, setPizzaCount,
+        handleclick2,
+        minusPizza2, plusPizza2 } = useContext(CartContext)
+
+    const [pizzaInCart, setPizzaInCart] = useState(null)
+
+
     useEffect(() => {
         getPizza(pizzaId)
     }, [])
 
+    useEffect(() => {
+        const index = cart.findIndex(ThisPizza => ThisPizza.id === pizza.id)
+        console.log(index)
+        index === -1 ? (setPizzaInCart(0)) : (setPizzaInCart(cart[index].count))
+    })
+
     return (
         <div className="d-flex justify-content-center w-100">
-            <div className="pizza-card d-flex flex-column align-items-center bg-success-subtle w-75 m-3 p-2 border">
+            <div className="pizza-card d-flex flex-column align-items-center bg-secondary-subtle w-75 m-3 p-2 border">
                 <h2 className="my-2">{pizza.name && <CapitalizeFirstLetter word={pizza.name} />}</h2>
                 <div className="img_container d-flex align-items-center justify-content-center m-2">
                     <img className="pizza-img" src={pizza.img} />
@@ -29,19 +44,32 @@ const Pizza = () => {
                         <ListGroup className="list-group list-group-flush">
                             {
                                 pizza.ingredients && pizza.ingredients.map((ingr, index) => (
-                                    <ListGroup.Item key={index} variant="success">{ingr}</ListGroup.Item>
+                                    <ListGroup.Item key={index} variant="secondary">{ingr}</ListGroup.Item>
                                 ))
                             }
                         </ListGroup>
                     </div>
                 </div>
                 <p className="mt-3 mb-5 w-75">{pizza.desc}</p>
-                <div >
-
-                    <Button variant="dark" className="d-flex align-items-center gap-2 fs-5 mb-2">
+                <div>
+                    {
+                        cart.findIndex(thisPizza => thisPizza.id === pizza.id) === -1 ? (
+                            <Button variant="dark" className="d-flex align-items-center gap-2 fs-5 mb-2" onClick={() => handleclick2(pizza.name, pizza.price, pizza.img, pizza.id)}>
+                                <p className="fs-4 text-success my-auto me-3 fw-bold text-center align-middle">${pizza.price && pizza.price.toLocaleString("es-ES", { useGrouping: true })}</p>Añadir
+                                <FontAwesomeIcon icon={faCartPlus} />
+                            </Button>
+                        ) : (
+                            <div className="button-container d-flex align-items-center gap-3 bg-white" >
+                                <Button variant="danger" className="fs-5 m-1" onClick={() => minusPizza2(pizza.price, pizza.id)}><FontAwesomeIcon icon={faMinus} /></Button>
+                                <span className="fs-5 mb-2 fw-bold">{pizzaInCart}</span>
+                                <Button variant="success" className="fs-5 m-1" onClick={() => plusPizza2(pizza.price, pizza.id)}><FontAwesomeIcon icon={faPlus} /></Button>
+                            </div>
+                        )
+                    }
+                    {/* <Button variant="dark" className="d-flex align-items-center gap-2 fs-5 mb-2">
                         <p className="fs-4 text-success my-auto me-3 fw-bold text-center align-middle">${pizza.price && pizza.price.toLocaleString("es-ES", { useGrouping: true })}</p>Añadir
                         <FontAwesomeIcon icon={faCartPlus} />
-                    </Button>
+                    </Button> */}
                 </div>
             </div>
         </div>
